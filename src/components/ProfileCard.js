4268dfc0a73bd1121f320a69f9b37111ba750725
4me7h <4573r10n@gmail.com>
2024-11-17 16:55:34 -0600

    setup: karla cv

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,31 +3,31 @@ import React from "react";
 import Photo from './Photo';
 import Content from './Content';
 
-import './ProfileCard.scss';
+
 // import img from '../images/img.jpg'
 // import img02 from '../images/img02.jpg'
-import img03 from '../images/img03.png'
+import img from '../images/img.jpg'
 
 
 const Card = () => {
     return (
         <section className="profile-card">
             <Photo 
-                photo = {img03}
+                photo = {img}
             />
             <Content 
-                name = "Ing. José Julian Tacias Rodriguez"
-                position = {["Director General"]}
-                company = "Avaluos y Servicios de Chiapas"
-                aboutText = {["Empresa líder en Chiapas en servicios de avalúos profesionales y consultoría especializada en bienes raíces."]}
-                email = "jj.taciasr@gmail.com"
-                phone = "961 235 2944"
-                whatsapp = "961 235 2944"
+                name = "Arquitecta Yotona"
+                position = {["Chofer de maquinaria de construcción"]}
+                company = "En el puente chiapas"
+                aboutText = {["A veces cuida a la helenita, le gusta comer palomitas y ver películas."]}
+                email = "yotona@gmail.com"
+                phone = "961 667 6381"
+                whatsapp = "961 667 6381"
                 whatsappmssg ="Estoy%20interesado%20en%20tu%20servicio"
                 facebook = "@AnaRodriguezMK"
                 facebookURL = "https://www.facebook.com/abraham.gomezjose" 
                 twitter = "@anarodriguez.marketing"  
-                address = "3ra. Poniente #624-A"
+                address = "Los niños, mun de la concordia"
             />
         </section>
     );
